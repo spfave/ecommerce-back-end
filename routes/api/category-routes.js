b47240e14faa433cb86ee8b2f3efbb3fed0e88a2
Sebastian F <sebpfave2@gmail.com>
2021-05-10 00:04:20 -0400

    Create category put route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,8 +48,22 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+// Update a category by its `id` value
+router.put("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+
+    if (!categoryData[0]) {
+      res.status(404).json({ message: `No category found for this id` });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 router.delete("/:id", (req, res) => {
