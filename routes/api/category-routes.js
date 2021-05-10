5baa48639b5a2f792b326807cf66278fe5122c31
Sebastian F <sebpfave2@gmail.com>
2021-05-09 23:55:18 -0400

    Create category post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,8 +38,14 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
-  // create a new category
+// Create a new category
+router.post("/", async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 router.put("/:id", (req, res) => {
