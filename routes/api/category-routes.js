e4a867e268f82b2d3fac2a562e5bfb56798df895
Sebastian F <sebpfave2@gmail.com>
2021-05-09 23:48:50 -0400

    Create category get single route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,9 +20,22 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// Find one category by its id with associated products
+router.get("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(400).json({ message: `No category found for this id` });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post("/", (req, res) => {
