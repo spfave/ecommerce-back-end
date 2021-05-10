1ea664333719b5e5b777805f1ca7b6a3aea330d6
Sebastian F <sebpfave2@gmail.com>
2021-05-09 21:06:04 -0400

    Define Model relations

--- models/index.js
+++ models/index.js
@@ -4,13 +4,26 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+// Product-Category relations
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Product-Tag relations
+Product.belongsToMany(Tag, {
+  through: { model: ProductTag, unique: false },
+  as: "product_tag",
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: { model: ProductTag, unique: false },
+  as: "tag_product",
+});
 
 module.exports = {
   Product,
