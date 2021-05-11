c3deefe2d7ede68f72de8570d92d6f26d012e976
Sebastian F <sebpfave2@gmail.com>
2021-05-10 22:07:01 -0400

    Create tag get by id route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,9 +20,21 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// Get one tag by its `id` with associated products
+router.get("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: "tag_products" }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: `No tag found for this id` });
+    }
+
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post("/", (req, res) => {
