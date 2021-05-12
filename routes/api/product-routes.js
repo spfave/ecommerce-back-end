0ce416f11345f0d13ca774162f485b2a6962cb87
Sebastian F <sebpfave2@gmail.com>
2021-05-12 17:17:23 -0400

    Remove commented out code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -73,27 +73,6 @@ router.post("/", async (req, res) => {
   } catch (error) {
     res.status(400).json(error);
   }
-
-  // Product.create(req.body)
-  //   .then((product) => {
-  //     // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-  //     if (req.body.tagIds.length) {
-  //       const productTagIdArr = req.body.tagIds.map((tag_id) => {
-  //         return {
-  //           product_id: product.id,
-  //           tag_id,
-  //         };
-  //       });
-  //       return ProductTag.bulkCreate(productTagIdArr);
-  //     }
-  //     // if no product tags, just respond
-  //     res.status(200).json(product);
-  //   })
-  //   .then((productTagIds) => res.status(200).json(productTagIds))
-  //   .catch((err) => {
-  //     console.log(err);
-  //     res.status(400).json(err);
-  //   });
 });
 
 // Update a product by its `id`
