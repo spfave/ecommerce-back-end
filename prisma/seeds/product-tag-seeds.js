5e782a0f639c9d510daa8b5db79164cb851f6adf
Sebastian F <sebpfdev@gmail.com>
2022-08-08 23:44:49 -0400

    Refactor seeding to use prisma

---
+++ prisma/seeds/product-tag-seeds.js
@@ -0,0 +1,52 @@
+const productTagData = [
+  {
+    productId: 1,
+    tagId: 6,
+  },
+  {
+    productId: 1,
+    tagId: 7,
+  },
+  {
+    productId: 1,
+    tagId: 8,
+  },
+  {
+    productId: 2,
+    tagId: 6,
+  },
+  {
+    productId: 3,
+    tagId: 1,
+  },
+  {
+    productId: 3,
+    tagId: 3,
+  },
+  {
+    productId: 3,
+    tagId: 4,
+  },
+  {
+    productId: 3,
+    tagId: 5,
+  },
+  {
+    productId: 4,
+    tagId: 1,
+  },
+  {
+    productId: 4,
+    tagId: 2,
+  },
+  {
+    productId: 4,
+    tagId: 8,
+  },
+  {
+    productId: 5,
+    tagId: 3,
+  },
+];
+
+module.exports = productTagData;
